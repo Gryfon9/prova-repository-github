6d52f41e8bc77695d1a6a2758811ade73a9d97a6
Trotten <Valerio@MacBook-Pro-di-Valerio.local>
2015-12-04 17:27:54 +0100

    cambiamento PEDERAFTA
    SEI UN PEDERAFTA

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -12,6 +12,7 @@ package javaapplication3;
 public class JavaApplication3 {
 
     /**
+     PEDERAFTA
      * @param args the command line arguments
      */
     public static void main(String[] args) {
